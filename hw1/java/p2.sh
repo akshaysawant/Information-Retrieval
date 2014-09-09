a79b4653dbbf6c550491d243edf7fd3b9af450e4
akshaysawant <akshaysawant@Akshays-MacBook-Pro.local>
2014-09-09 00:06:54 -0400

    Deleted IR

--- hw1/java/p2.sh
+++
@@ -1,3 +0,0 @@
-
-javac -cp jsoup.jar  file.java
-java -cp  .:jsoup.jar crawler
--- hw1/p1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget -r www.ccs.neu.edu -U firefox -S --spider -A pdf  -w 5 -o out.txt
-
-grep -e "--  http" out.txt | cut -d " " -f 4 | awk '!_[$1]++' | head -n 100 > p1.txt
-
-rm -rf out.txt www.ccs.neu.edu
